0f344e5ea9679f2a945df53323a0f9d0ff5871fc
Archie61 <arturfv@hotmail.com>
2025-12-01 00:52:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN =8185190513: "AAHHPkNqlxkQfVnOLOXJ5C3h9U1nCTqzYVY"   # ← ВСТАВЬТЕ ВАШ ТОКЕН
+BOT_TOKEN = "AAHHPkNqlxkQfVnOLOXJ5C3h9U1nCTqzYVY"   # ← ВСТАВЬТЕ ВАШ ТОКЕН
 MANAGER_ID = 521492501  # ← ВСТАВЬТЕ ID ЖЕНЫ
