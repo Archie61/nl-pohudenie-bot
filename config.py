ece3323b713297e0aa5169332ee5a6f3cdae6a53
Archie61 <arturfv@hotmail.com>
2025-11-30 11:45:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 BOT_TOKEN = "8185190513:AAHHPkNqlxkQfVnOLOXJ5C3h9U1nCTqzYVY"  # ← ВСТАВЬТЕ ВАШ ТОКЕН
-MANAGER_ID = 007-3688700  # ← ВСТАВЬТЕ ID ЖЕНЫ
+MANAGER_ID = 521492501  # ← ВСТАВЬТЕ ID ЖЕНЫ
 
