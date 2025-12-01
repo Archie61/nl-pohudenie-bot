aabf98037059c2921db3929c468c04d5b2928568
Archie61 <arturfv@hotmail.com>
2025-12-01 03:24:13 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,8 @@
+from . import start
+from . import auth
+from . import catalog
+from . import partner
+from . import help as help_handler
+from . import support
 
+__all__ = ['start', 'auth', 'catalog', 'partner', 'help_handler', 'support']
